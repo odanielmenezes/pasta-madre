70358c2488bcb6ef3f1df242dfdf637185e8fa80
Daniel Menezes <daniel.menezes@infoar.com.br>
2022-12-12 19:38:09 -0300

    remove route

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./views/home/index";
 import { SenderEmail } from "./components/form-email/component";
 
 export function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" exact element={<HomePage />} />
-        <Route path="/orcamento" element={<SenderEmail />} />
-      </Routes>
-    </Router>
+    <>
+      <HomePage />
+      <SenderEmail />
+    </>
   );
 }
